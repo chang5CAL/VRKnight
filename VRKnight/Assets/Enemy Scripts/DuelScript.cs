dfaaf86b7749a02d116fdba575ba6e0f2f7b9906
Justin <ginka13@gmail.com>
2016-12-27 12:44:02 -0800

    Added Google VR SDK, added summary for manual scripts

---
+++ VRKnight/Assets/Enemy Scripts/DuelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuelScript : MonoBehaviour {
+    /// <summary>
+    /// This one controls the enemy's AI in duel mode. 
+    /// What if does is basically have a block animation
+    /// when attacked, UNLESS staggered. If one of the
+    /// enemy's attack is blocked, they stagger.
+    /// They also have randomized attacks, like in
+    /// offense script.
+    /// </summary>
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VRKnight/Assets/Enemy Scripts/OffenseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffenseScript : MonoBehaviour {
+    /// <summary>
+    /// This controls the enemy AI in defense mode.
+    /// It'll just keep attacking every few seconds, 
+    /// with the frequency increasing as time goes on.
+    /// Does not stagger on successful block.
+    /// </summary>
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
