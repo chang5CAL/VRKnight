5fd4ede6b1da41fa0c3c2f3ea211ed9c6bbd3bd0
Justin <ginka13@gmail.com>
2017-01-07 00:53:13 -0800

    Added HP and started applying it to collision

--- Assets/Player Scripts/ViewScript.cs
+++ Assets/Player Scripts/ViewScript.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewScript : MonoBehaviour {
-
+    int hp;
 	// Use this for initialization
 	void Start () {
-		
+        hp = 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(Input.mousePosition);
+        if (hp <= 0)
+        {
+            //Game Over
+        }
 	}
 
-    void onCollision()
+    void onCollisionEnter (Collision col)
     {
+        if (col.gameObject.name == "Enemy")
+        {
+            //Subtract health
+            hp--;
+        }
 
     }
 }
