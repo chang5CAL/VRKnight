e743add1031e6e799fe3b455fde0965daac44b09
Justin <ginka13@gmail.com>
2017-01-08 13:10:54 -0800

    Moved collision code to player, adjusted player position

--- Assets/Player Scripts/PlayerScript.cs
+++ Assets/Player Scripts/PlayerScript.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    int hp;
+    // Use this for initialization
+    void Start ()
+    {
+        hp = 3;
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (hp <= 0)
+        {
+            //Game Over
+        }
 
-	}
+    }
+
+    void onCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Enemy")
+        {
+            //Subtract health
+            hp--;
+        }
+
+    }
 }
--- Assets/Player Scripts/ViewScript.cs
+++ Assets/Player Scripts/ViewScript.cs
@@ -3,28 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewScript : MonoBehaviour {
-    int hp;
 	// Use this for initialization
 	void Start () {
-        hp = 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(Input.mousePosition);
-        if (hp <= 0)
-        {
-            //Game Over
-        }
 	}
 
-    void onCollisionEnter (Collision col)
-    {
-        if (col.gameObject.name == "Enemy")
-        {
-            //Subtract health
-            hp--;
-        }
-
-    }
 }
