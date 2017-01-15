1253a84d7a4db069708c068444958902e42546d6
Justin <ginka13@gmail.com>
2017-01-14 16:33:09 -0800

    added score in crosshair

--- Assets/Player Scripts/CrosshairScript.cs
+++ Assets/Player Scripts/CrosshairScript.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CrosshairScript : MonoBehaviour {
-
+    int score;
 	// Use this for initialization
 	void Start () {
-		
+        score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
+    void onCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Enemy")
+        {
+            score++;
+        }
+
+    }
 }
