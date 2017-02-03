5378f39d2a2b15c1484316d38322690f97f345b3
Justin <ginka13@gmail.com>
2017-02-03 04:32:59 -0800

    Added offense script info to duel script for later use

--- Assets/Enemy Scripts/DuelScript.cs
+++ Assets/Enemy Scripts/DuelScript.cs
@@ -11,14 +11,248 @@ public class DuelScript : MonoBehaviour {
     /// They also have randomized attacks, like in
     /// offense script.
     /// </summary>
+    bool isActive;
+    int chain;
+    int followUp;
+    bool coolDown;
+    public int score;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        isActive = false;
+        coolDown = false; //If the player is hit, set to true.
+        chain = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rand;
+        System.Random rnd = new System.Random();
+        if (Time.fixedDeltaTime % 5 == 0)
+        {
+            chain++;
+            isActive = true;
+            rand = rnd.Next(5);
+            if (rand == 0)
+            {
+                SlashLeft();
+            }
+            else if (rand == 1)
+            {
+                Thrust();
+            }
+            else if (rand == 2)
+            {
+                SlashLeftQuick();
+            }
+            else if (rand == 3)
+            {
+                SlashRight();
+            }
+            else if (rand == 4)
+            {
+                SlashRightQuick();
+            }
+            //Do an attack.
+        }
+        isActive = false;
+    }
+    void SlashLeft()
+    {
+
+        score += 1;
+        System.Random followUp = new System.Random();
+        int follow = followUp.Next(chain);
+        if (follow > chain * 3)
+        {
+            //Basically, get a random number and if it divided by the chain 
+            //is equal to a certain amount (Probably decided by time), increase
+            //The chance of the enemy to do another attack immediately instead of
+            //waiting.
+            chain++;
+            int rand;
+            System.Random rnd = new System.Random();
+            rand = rnd.Next(5);
+            if (rand == 0)
+            {
+                SlashLeft();
+            }
+            else if (rand == 1)
+            {
+                Thrust();
+            }
+            else if (rand == 2)
+            {
+                SlashLeftQuick();
+            }
+            else if (rand == 3)
+            {
+                SlashRight();
+            }
+            else if (rand == 4)
+            {
+                SlashRightQuick();
+            }
+        }
+
+    }
+    void Thrust()
+    {
+        score += 2;
+        int rand;
+        System.Random rnd = new System.Random();
+        rand = rnd.Next(3);
+        if (rand == 0)
+        {
+            //Thrust High
+        }
+        else if (rand == 1)
+        {
+            //Thrust Mid
+        }
+        else
+        {
+            //Thrust Low
+        }
+
+
+        System.Random followUp = new System.Random();
+        int follow = followUp.Next(score);
+        if (follow > chain * 3)
+        {
+            chain++;
+            rand = rnd.Next(5);
+            if (rand == 0)
+            {
+                SlashLeft();
+            }
+            else if (rand == 1)
+            {
+                Thrust();
+            }
+            else if (rand == 2)
+            {
+                SlashLeftQuick();
+            }
+            else if (rand == 3)
+            {
+                SlashRight();
+            }
+            else if (rand == 4)
+            {
+                SlashRightQuick();
+            }
+        }
+
+    }
+    void SlashLeftQuick()
+    {
+        score += 2;
+        System.Random followUp = new System.Random();
+        int follow = followUp.Next(score);
+        if (follow > chain * 3)
+        {
+            chain++;
+            int rand;
+            System.Random rnd = new System.Random();
+            rand = rnd.Next(5);
+            if (rand == 0)
+            {
+                SlashLeft();
+            }
+            else if (rand == 1)
+            {
+                Thrust();
+            }
+            else if (rand == 2)
+            {
+                SlashLeftQuick();
+            }
+            else if (rand == 3)
+            {
+                SlashRight();
+            }
+            else if (rand == 4)
+            {
+                SlashRightQuick();
+            }
+        }
+
+    }
+    void SlashRight()
+    {
+        score += 1;
+
+        System.Random followUp = new System.Random();
+        int follow = followUp.Next(score);
+        if (follow > chain * 3)
+        {
+            //Basically, get a random number and if it divided by the chain 
+            //is equal to a certain amount (Probably decided by time), increase
+            //The chance of the enemy to do another attack immediately instead of
+            //waiting.
+            chain++;
+            int rand;
+            System.Random rnd = new System.Random();
+            rand = rnd.Next(5);
+            if (rand == 0)
+            {
+                SlashLeft();
+            }
+            else if (rand == 1)
+            {
+                Thrust();
+            }
+            else if (rand == 2)
+            {
+                SlashLeftQuick();
+            }
+            else if (rand == 3)
+            {
+                SlashRight();
+            }
+            else if (rand == 4)
+            {
+                SlashRightQuick();
+            }
+        }
+
+    }
+    void SlashRightQuick()
+    {
+        score += 2;
+
+        System.Random followUp = new System.Random();
+        int follow = followUp.Next(score);
+        if (follow > chain * 3)
+        {
+            chain++;
+            int rand;
+            System.Random rnd = new System.Random();
+            rand = rnd.Next(5);
+            if (rand == 0)
+            {
+                SlashLeft();
+            }
+            else if (rand == 1)
+            {
+                Thrust();
+            }
+            else if (rand == 2)
+            {
+                SlashLeftQuick();
+            }
+            else if (rand == 3)
+            {
+                SlashRight();
+            }
+            else if (rand == 4)
+            {
+                SlashRightQuick();
+            }
+        }
+
+    }
 }
