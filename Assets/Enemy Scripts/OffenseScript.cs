303f7e1ab25e4347f25ff07547ddee5fbdbea56b
Justin <ginka13@gmail.com>
2017-02-16 01:12:34 -0800

    Started adding more for thrust animations

--- Assets/Enemy Scripts/OffenseScript.cs
+++ Assets/Enemy Scripts/OffenseScript.cs
@@ -194,7 +194,7 @@ public class OffenseScript : MonoBehaviour {
     {
         score += 3;
         float startTime = Time.deltaTime;
-        while (Time.deltaTime < startTime + 3)
+        while (Time.deltaTime < startTime + 1)
         {
             //Here, take 5 seconds to put sword back in place. Probably too long.
             /*
@@ -216,14 +216,62 @@ public class OffenseScript : MonoBehaviour {
         if (rand == 0)
         {
             //Thrust High
+            while (Time.deltaTime < startTime + 3)
+            {
+                //Here, take 5 seconds to put sword back in place. Probably too long.
+                /*
+                transform.position = new Vector3(transform.position.x - transform.position.x / 2 + .53f,
+                    transform.position.y - transform.position.y / 2 + .87f,
+                    transform.position.z - transform.position.z / 2);
+                */
+
+                transform.rotation = new Quaternion(transform.rotation.x - transform.rotation.x / 2,
+                    transform.rotation.y,
+                    transform.rotation.z,
+                    transform.rotation.w);
+                //So I need to figure out which axis I need to rotate around I assume X, so I'm
+                //removing the rotation except for the X.
+            }
         }
         else if(rand == 1)
         {
             //Thrust Mid
+            while (Time.deltaTime < startTime + 3)
+            {
+                //Here, take 5 seconds to put sword back in place. Probably too long.
+                /*
+                transform.position = new Vector3(transform.position.x - transform.position.x / 2 + .53f,
+                    transform.position.y - transform.position.y / 2 + .87f,
+                    transform.position.z - transform.position.z / 2);
+                */
+
+                transform.rotation = new Quaternion(transform.rotation.x - transform.rotation.x / 2,
+                    transform.rotation.y,
+                    transform.rotation.z,
+                    transform.rotation.w);
+                //So I need to figure out which axis I need to rotate around I assume X, so I'm
+                //removing the rotation except for the X.
+            }
         }
         else
         {
             //Thrust Low
+            while (Time.deltaTime < startTime + 3)
+            {
+                //Here, take 5 seconds to put sword back in place. Probably too long.
+                /*
+                transform.position = new Vector3(transform.position.x - transform.position.x / 2 + .53f,
+                    transform.position.y - transform.position.y / 2 + .87f,
+                    transform.position.z - transform.position.z / 2);
+                */
+
+                transform.rotation = new Quaternion(transform.rotation.x - transform.rotation.x / 2,
+                    transform.rotation.y,
+                    transform.rotation.z,
+                    transform.rotation.w);
+                //So I need to figure out which axis I need to rotate around I assume X, so I'm
+                //removing the rotation except for the X.
+            }
         }
 
 
