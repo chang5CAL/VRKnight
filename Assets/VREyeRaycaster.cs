ef829017dbaa389580c78d506b9b81383b5b24a0
Vyskan <ginka13@gmail.com>
2017-06-04 22:03:32 -0700

    Started to copy from demos

--- Assets/VREyeRaycaster.cs
+++ Assets/VREyeRaycaster.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VREyeRaycaster : MonoBehaviour {
-
+    public event system.Action OnOver;
 	// Use this for initialization
 	void Start () {
 		
